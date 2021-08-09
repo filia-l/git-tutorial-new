828dabc13635575080247e75e80fe1c7f820bf2d
Liubov Moshynska <ext-liubov.moshynska@here.com>
2021-08-09 18:36:29 +0300

    Initial commit

---
+++ src/main/java/com/intellias/ideademo/MyApp.java
@@ -0,0 +1,14 @@
+package com.intellias.ideademo;
+
+import java.time.DayOfWeek;
+import java.util.Calendar;
+
+public class MyApp {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+
+        Calendar.DAY_OF_WEEK();
+        DayOfWeek.SATURDAY
+    }
+}
+
